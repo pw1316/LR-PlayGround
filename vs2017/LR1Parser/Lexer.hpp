7826c9f60607045bc72385f3af043b64ef08a15f
Joker Yough <of19950402@gmail.com>
2018-09-11 18:09:23 +0800

    #1 Generate Token Stream

--- vs2017/LR1Parser/Lexer.hpp
+++ vs2017/LR1Parser/Lexer.hpp
@@ -1,6 +1,63 @@
 #pragma once
+#include <queue>
+#include <regex>
+#include <string>
+
+#include <iostream>
+
 #include <Token.hpp>
 
 namespace LR::Lexer
 {
+    class Lexer
+    {
+    public:
+        static std::queue<Token::ElementSymbol> Lex(const std::string input)
+        {
+            std::queue<Token::ElementSymbol> ret;
+            std::vector<std::regex> token_reg(Token::TOKEN_VALUE.size());
+            for (size_t i = 0; i < token_reg.size(); ++i)
+            {
+                token_reg[i] = Token::TOKEN_VALUE[i];
+            }
+
+            auto iter = input.begin();
+            Token::ElementSymbol symbolCache;
+            symbolCache.header.type = Token::ElementType::Symbol;
+            symbolCache.type = Token::SymbolType::NONE;
+            while (iter != input.end())
+            {
+                size_t i = 0;
+                for (i = 0; i < token_reg.size(); ++i)
+                {
+                    std::smatch ms;
+                    if (std::regex_search(iter, input.end(), ms, token_reg[i], std::regex_constants::match_continuous))
+                    {
+                        symbolCache.header.type = Token::ElementType::Symbol;
+                        symbolCache.type = static_cast<Token::SymbolType>(i + 1);
+                        PushSymbol(ret, symbolCache);
+                        iter += ms[0].length();
+                        break;
+                    }
+                }
+                /* Failed */
+                if (i == token_reg.size())
+                {
+                    return std::queue<Token::ElementSymbol>();
+                }
+            }
+            return ret;
+        }
+    private:
+        static void PushSymbol(std::queue<Token::ElementSymbol> &q, Token::ElementSymbol &symbol)
+        {
+            if (symbol.type == Token::SymbolType::NONE)
+            {
+                return;
+            }
+            q.push(symbol);
+            symbol.header.type = Token::ElementType::Symbol;
+            symbol.type = Token::SymbolType::NONE;
+        }
+    };
 }
--- vs2017/LR1Parser/Main.cpp
+++ vs2017/LR1Parser/Main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <queue>
+
+#include <Lexer.hpp>
 
 int main()
 {
-    std::cout << "TODO\n";
+    auto q = LR::Lexer::Lexer::Lex("(1*(4+5+2)-3)*(6+8)");
+    std::cout << "TODO First Set\n";
 }
